91baf5e0817d8de5904a91174d4bda9b239fcc0c
Sylvain Brochard <sbro@me.com>
2022-04-15 13:07:49 +0200

    autovoting in background works

--- src/components/ChallengeCard.js
+++ src/components/ChallengeCard.js
@@ -13,10 +13,13 @@ export default function ChallengeCard({ challenge }) {
 
     const needVote = challenge.member.ranking.exposure.exposure_factor < 100
 
+    const boostState = challenge.member.boost.state
+    const boostAvailable = boostState !== 'USED' && boostState !== 'EXPIRED' && boostState !== 'LOCKED'
+
     return (
         <View style={{ flexDirection: 'row', backgroundColor: needVote ? '#efcf4c' : isDark ? 'black' : 'white' }} >
             <Image source={{ uri: `https://photos.gurushots.com//unsafe/500x500/${member.id}/3_${id}.jpg` }} style={{ width: 116, height: 116, marginRight: 10 }} />
-            <View style={{ padding: 16 }}>
+            <View style={{ padding: 16, borderColor: boostAvailable ? 'green' : 'white', borderWidth: boostAvailable ? 2 : 0 }}>
                 <Text style={{ fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>{challenge.title}</Text>
                 <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Exposure factor: {challenge.member.ranking.exposure.exposure_factor}</Text>
                 <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Rank: {memberScore.level_name} ({memberScore.next_message})</Text>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,34 +2,62 @@ import { View, Text, FlatList, TouchableOpacity, RefreshControl, Appearance, Act
 import React, { useEffect, useState } from 'react'
 import { fetchChallengesAndVote, getActiveChallenges } from '../utils/gurushotsApi'
 import ChallengeCard from '../components/ChallengeCard'
-import BackgroundTimer from 'react-native-background-timer';
+import queue, { Worker } from 'react-native-job-queue'
+import moment from 'moment'
 
 export default function Home() {
     const [challenges, setChallenges] = useState({})
     const [loading, setLoading] = useState(true)
     const [refreshing, setRefreshing] = useState(false)
     const [voting, setVoting] = useState(false)
-    const [voteScheduleInterval, setVoteScheduleInterval] = useState(15 * 60 * 1000)
+    const [voteScheduleInterval, setVoteScheduleInterval] = useState(5 * 60 * 1000)
+    const [recursionCounter, setRecursionCounter] = useState(0)
     const [isAutoVoteEnabled, setIsAutoVoteEnabled] = useState(false)
+    const [nextVoteTimeStamp, setNextVoteTimeStamp] = useState(0)
     const colorScheme = Appearance.getColorScheme();
     const isDark = colorScheme === 'dark';
 
-    const voteScheduler = BackgroundTimer;
 
-    const enableVoteSchedule = () => {
+    useEffect(() => {
+        queue.configure({
+            onQueueFinish: (executedJobs) => {
+                console.log("Queue stopped and executed", executedJobs)
+            }
+        });
+
+
+        const registeredWorkers = queue.registeredWorkers;
+        if (!registeredWorkers['do-autovote']) {
+            queue.addWorker(new Worker("do-autovote", async (payload) => {
+                return await new Promise((resolve) => {
+                    setTimeout(() => {
+                        voteToAllChallenges()
+                        setIsAutoVoteEnabled(true)
+                        setRecursionCounter(recursionCounter + 1)
+                        queue.addJob('do-autovote', {});
+                        setNextVoteTimeStamp(Date.now() + voteScheduleInterval)
+                        resolve();
+                    }, voteScheduleInterval);
+                });
+            }))
+        }
+    }, [])
+
+    const startAutoVote = () => {
         setIsAutoVoteEnabled(true)
-        voteScheduler.runBackgroundTimer(() => {
-            setIsAutoVoteEnabled(true)
-            voteToAllChallenges()
-        },
-            voteScheduleInterval);
+        setNextVoteTimeStamp(Date.now() + voteScheduleInterval)
+        voteToAllChallenges()
+        setRecursionCounter(recursionCounter + 1)
+        queue.addJob('do-autovote', {})
+        queue.start();
     }
 
-
-    const disableVoteSchedule = () => {
-        voteScheduler.stopBackgroundTimer();
+    const stopAutoVote = () => {
         setIsAutoVoteEnabled(false)
+        setRecursionCounter(0)
+        queue.stop()
     }
+
 
     useEffect(() => {
         if (!challenges.challenges) {
@@ -54,17 +82,30 @@ export default function Home() {
         })
     }
 
-    console.log(challenges)
-
-    const header = (<View style={{ flexDirection: 'row', height: 60, justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 8 }}>
-        <Text style={{ fontSize: 18, fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>
-            {loading || refreshing ? voting ? 'Voting...' : 'Loading challenges...' : `Active Challenge${challenges.challenges.length > 1 ? 's' : ''}`}
-        </Text>
-        <TouchableOpacity onPress={() => isAutoVoteEnabled ? disableVoteSchedule() : enableVoteSchedule()} style={{ flexDirection: 'row' }}>
-            <ActivityIndicator size="small" color={isDark ? 'white' : 'black'} animating={isAutoVoteEnabled} />
-            <Text style={{ fontSize: 18, color: isAutoVoteEnabled ? 'green' : 'red', marginLeft: 4 }}>{isAutoVoteEnabled ? 'Autovote on' : 'Stopped'}</Text>
-        </TouchableOpacity>
-    </View>)
+    const header = (
+        <View style={{ paddingHorizontal: 8, marginVertical: 12 }}>
+            <View>
+                <View style={{ flexDirection: 'row', height: 60, justifyContent: 'space-between', alignItems: 'center', }}>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>
+                        {loading || refreshing ? voting ? isAutoVoteEnabled ? 'Autovoting...' : 'Voting...' : 'Loading challenges...' : `Active Challenge${challenges.challenges.length > 1 ? 's' : ''}`}
+                    </Text>
+                    <TouchableOpacity onPress={() => isAutoVoteEnabled ? stopAutoVote() : startAutoVote()} style={{ flexDirection: 'row' }}>
+                        <Text style={{ fontSize: 18, color: isAutoVoteEnabled ? 'red' : 'green', marginLeft: 4 }}>{isAutoVoteEnabled ? 'Stop' : 'Start'}</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ color: isAutoVoteEnabled ? 'green' : 'red' }}>Autovoter {isAutoVoteEnabled ? 'enabled' : 'disabled'} </Text>
+                    <ActivityIndicator size='small' color='green' animating={isAutoVoteEnabled} />
+                </View>
+                {isAutoVoteEnabled && <View>
+                    <Text style={{ color: isDark ? 'white' : 'black' }}>Voted {recursionCounter} times</Text>
+                    <Text style={{ color: isDark ? 'white' : 'black' }}>Next vote at {moment(moment.unix(nextVoteTimeStamp / 1000)).local().format('HH:mm:ss')}</Text>
+                </View>}
+            </View>
+        </View>
+    )
 
     const footer = !loading && (<TouchableOpacity
         style={{ marginVertical: 20, padding: 20, justifyContent: 'center', alignItems: 'center', backgroundColor: loading ? 'grey' : 'coral' }}
