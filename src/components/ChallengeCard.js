0cc1b50d4848c225c09b1934282caa039e3e0bb4
Sylvain Brochard <sbro@me.com>
2022-04-16 21:47:19 +0200

    removed queue system and added individual voting feature

--- src/components/ChallengeCard.js
+++ src/components/ChallengeCard.js
@@ -1,9 +1,12 @@
-import { View, Text, Image, Appearance } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Appearance, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
 import moment from 'moment'
+import { getVoteImages, submitVotes } from '../utils/gurushotsApi'
 
 
-export default function ChallengeCard({ challenge }) {
+export default function ChallengeCard({ challenge, forceRefresh }) {
+
+    const [isVoting, setIsVoting] = useState(false)
 
     const colorScheme = Appearance.getColorScheme();
     const isDark = colorScheme === 'dark';
@@ -16,16 +19,44 @@ export default function ChallengeCard({ challenge }) {
     const boostState = challenge.member.boost.state
     const boostAvailable = boostState !== 'USED' && boostState !== 'EXPIRED' && boostState !== 'LOCKED'
 
+    const vote = async () => {
+        setIsVoting(true)
+        const voteImages = await getVoteImages(challenge)
+        submitVotes(voteImages).then((res) => {
+            forceRefresh(true)
+            setIsVoting(false)
+        })
+    }
+
     return (
-        <View style={{ flexDirection: 'row', backgroundColor: needVote ? '#efcf4c' : isDark ? 'black' : 'white' }} >
-            <Image source={{ uri: `https://photos.gurushots.com//unsafe/500x500/${member.id}/3_${id}.jpg` }} style={{ width: 116, height: 116, marginRight: 10 }} />
-            <View style={{ padding: 16, borderColor: boostAvailable ? 'green' : 'white', borderWidth: boostAvailable ? 2 : 0 }}>
-                <Text style={{ fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>{challenge.title}</Text>
-                <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Exposure factor: {challenge.member.ranking.exposure.exposure_factor}</Text>
-                <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Rank: {memberScore.level_name} ({memberScore.next_message})</Text>
-                <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Ends at: {moment.unix(challenge.close_time).format('YYYY-MM-DD HH:MM')}</Text>
-                <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Your score: {memberScore.votes} votes</Text>
+        <View>
+            <View style={{ flexDirection: 'row', backgroundColor: needVote ? '#efcf4c' : isDark ? 'black' : 'white' }} >
+                <Image source={{ uri: `https://photos.gurushots.com//unsafe/500x500/${member.id}/3_${id}.jpg` }} style={{ width: 116, height: 116, marginRight: 10 }} />
+                <View style={{ padding: 16, borderColor: boostAvailable ? 'green' : 'white', borderWidth: boostAvailable ? 2 : 0 }}>
+                    <Text style={{ fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>{challenge.title}</Text>
+                    <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Exposure factor: {challenge.member.ranking.exposure.exposure_factor}</Text>
+                    <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Rank: {memberScore.level_name} ({memberScore.next_message})</Text>
+                    <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Ends at: {moment.unix(challenge.close_time).format('YYYY-MM-DD HH:MM')}</Text>
+                    <Text style={{ fontSize: 12, color: isDark ? 'white' : 'black' }}>Your score: {memberScore.votes} votes</Text>
+                </View>
+
+                {challenge.member.ranking.exposure.exposure_factor < 100 && <TouchableOpacity
+                    onPress={() => vote()}
+                    style={{
+                        position: 'absolute',
+                        right: 4,
+                        bottom: 4,
+                        padding: 4,
+                        backgroundColor: 'green',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: 4
+                    }}
+                    disabled={isVoting}>
+                    <Text style={{ color: 'white', fontSize: 8 }}>Vote </Text>
+                </TouchableOpacity>}
+
             </View>
-        </View>
+        </View >
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,73 +2,29 @@ import { View, Text, FlatList, TouchableOpacity, RefreshControl, Appearance, Act
 import React, { useEffect, useState } from 'react'
 import { fetchChallengesAndVote, getActiveChallenges } from '../utils/gurushotsApi'
 import ChallengeCard from '../components/ChallengeCard'
-import queue, { Worker } from 'react-native-job-queue'
-import moment from 'moment'
 
 export default function Home() {
     const [challenges, setChallenges] = useState({})
     const [loading, setLoading] = useState(true)
     const [refreshing, setRefreshing] = useState(false)
     const [voting, setVoting] = useState(false)
-    const [voteScheduleInterval, setVoteScheduleInterval] = useState(5 * 60 * 1000)
-    const [recursionCounter, setRecursionCounter] = useState(0)
-    const [isAutoVoteEnabled, setIsAutoVoteEnabled] = useState(false)
-    const [nextVoteTimeStamp, setNextVoteTimeStamp] = useState(0)
     const colorScheme = Appearance.getColorScheme();
+    const [forceRefresh, setForceRefresh] = useState(false)
     const isDark = colorScheme === 'dark';
 
 
-    useEffect(() => {
-        queue.configure({
-            onQueueFinish: (executedJobs) => {
-                console.log("Queue stopped and executed", executedJobs)
-            }
-        });
-
-
-        const registeredWorkers = queue.registeredWorkers;
-        if (!registeredWorkers['do-autovote']) {
-            queue.addWorker(new Worker("do-autovote", async (payload) => {
-                return await new Promise((resolve) => {
-                    setTimeout(() => {
-                        voteToAllChallenges()
-                        setIsAutoVoteEnabled(true)
-                        setRecursionCounter(recursionCounter + 1)
-                        queue.addJob('do-autovote', {});
-                        setNextVoteTimeStamp(Date.now() + voteScheduleInterval)
-                        resolve();
-                    }, voteScheduleInterval);
-                });
-            }))
-        }
-    }, [])
-
-    const startAutoVote = () => {
-        setIsAutoVoteEnabled(true)
-        setNextVoteTimeStamp(Date.now() + voteScheduleInterval)
-        voteToAllChallenges()
-        setRecursionCounter(recursionCounter + 1)
-        queue.addJob('do-autovote', {})
-        queue.start();
-    }
-
-    const stopAutoVote = () => {
-        setIsAutoVoteEnabled(false)
-        setRecursionCounter(0)
-        queue.stop()
-    }
-
 
     useEffect(() => {
-        if (!challenges.challenges) {
+        if (!challenges.challenges || forceRefresh) {
             getActiveChallenges()
                 .then(challenges => {
                     setChallenges(challenges)
                     setLoading(false)
                     setRefreshing(false)
                 })
+            setForceRefresh(false)
         }
-    }, [challenges])
+    }, [challenges, forceRefresh])
 
     const voteToAllChallenges = async () => {
         setVoting(true)
@@ -84,25 +40,10 @@ export default function Home() {
 
     const header = (
         <View style={{ paddingHorizontal: 8, marginVertical: 12 }}>
-            <View>
-                <View style={{ flexDirection: 'row', height: 60, justifyContent: 'space-between', alignItems: 'center', }}>
-                    <Text style={{ fontSize: 18, fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>
-                        {loading || refreshing ? voting ? isAutoVoteEnabled ? 'Autovoting...' : 'Voting...' : 'Loading challenges...' : `Active Challenge${challenges.challenges.length > 1 ? 's' : ''}`}
-                    </Text>
-                    <TouchableOpacity onPress={() => isAutoVoteEnabled ? stopAutoVote() : startAutoVote()} style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 18, color: isAutoVoteEnabled ? 'red' : 'green', marginLeft: 4 }}>{isAutoVoteEnabled ? 'Stop' : 'Start'}</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={{ color: isAutoVoteEnabled ? 'green' : 'red' }}>Autovoter {isAutoVoteEnabled ? 'enabled' : 'disabled'} </Text>
-                    <ActivityIndicator size='small' color='green' animating={isAutoVoteEnabled} />
-                </View>
-                {isAutoVoteEnabled && <View>
-                    <Text style={{ color: isDark ? 'white' : 'black' }}>Voted {recursionCounter} times</Text>
-                    <Text style={{ color: isDark ? 'white' : 'black' }}>Next vote at {moment(moment.unix(nextVoteTimeStamp / 1000)).local().format('HH:mm:ss')}</Text>
-                </View>}
+            <View style={{ flexDirection: 'row', height: 60, justifyContent: 'space-between', alignItems: 'center', }}>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: isDark ? 'white' : 'black' }}>
+                    {loading || refreshing ? voting ? 'Voting...' : 'Loading challenges...' : `Active Challenge${challenges.challenges.length > 1 ? 's' : ''}`}
+                </Text>
             </View>
         </View>
     )
@@ -121,7 +62,7 @@ export default function Home() {
         <View style={{ flex: 1 }}>
             <FlatList
                 data={challenges.challenges ? challenges.challenges.sort((a, b) => a.close_time - b.close_time) : []}
-                renderItem={({ item }) => <ChallengeCard challenge={item} />}
+                renderItem={({ item }) => <ChallengeCard challenge={item} forceRefresh={(b) => setForceRefresh(b)} />}
                 keyExtractor={item => item.id}
                 ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
                 ListHeaderComponent={header}
